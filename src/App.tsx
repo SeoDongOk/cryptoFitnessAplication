9cb3e4aa6643b5e8d73a2f82b7d971310966430b
DongOk <hcan159@naver.com>
2023-04-09 00:51:37 +0900

    base_0409

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@scenes/navbar"
+
+
+function App() {
+
+  return (
+    <div className="app bg-gray-20 ">
+      <Navebar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+useEffect 
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
