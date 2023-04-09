d61d314471609b2776a587e5d9ec2e043e6f4b55
DongOk <hcan159@naver.com>
2023-04-09 23:53:02 +0900

    add Logo
    adding algo in baseic type.
    
    problem is @heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@scenes/navbar"
+import Navbar from "./scenes/navbar/index";
 
 
 function App() {
 
   return (
     <div className="app bg-gray-20 ">
-      <Navebar />
+      <Navbar />
     </div>
   )
 }
